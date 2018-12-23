7519cf19af272cb30f6fbd3d090755647cfa9f19
olaborde <oml007@live.com>
2018-12-23 12:08:59 -0500

    Established a One-to-Many Relationship between the cheese and the category tables

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,13 +1,27 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
+
+
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
 
     public Category() {
 
@@ -16,10 +30,6 @@ public class Category {
     public Category(String name) {
         this.name = name;
     }
-
-    @Id
-    @GeneratedValue
-    private int id;
 
     public int getId() {
         return id;
@@ -33,7 +43,7 @@ public class Category {
         this.name = name;
     }
 
-    @NotNull
-    @Size(min=3, max=15)
-    private String name;
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
 }
--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -3,6 +3,7 @@ package org.launchcode.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -24,11 +25,21 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+//    private CheeseType type;
+    @ManyToOne
+    private Category category;
 
     public Cheese(String name, String description) {
         this.name = name;
         this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     public Cheese() { }
@@ -53,11 +64,11 @@ public class Cheese {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
-    }
-
-    public void setType(CheeseType type) {
-        this.type = type;
-    }
+//    public CheeseType getType() {
+//        return type;
+//    }
+//
+//    public void setType(CheeseType type) {
+//        this.type = type;
+//    }
 }
